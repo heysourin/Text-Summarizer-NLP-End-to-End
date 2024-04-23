60ffea9907734685344d06ef41e199ef794e17a4
heysourin <sourin07@gmail.com>
2024-04-24 00:06:42 +0530

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Text-Summarizer-NLP-End-to-End
 End to end text summarizer Natural Language Processing (NLP) project with CI/CD implementation on AWS.
+
+# End-to-End-ML-project
+
+```bash
+python3 -m venv myenv
+```
+
+```bash
+source myenv/bin/activate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python3 app.py
+```
+
+```bash
+Now open up your local host 0.0.0.0:8080
+```
+
+```bash
+deactivate
+```
+- Sometimes 'deactivate' file permission is not givem, then:
+```bash
+cd /home/ryle/.vscode/extensions/ms-python.python-2024.5.11021008/python_files/deactivate/bash/
+```
+```bash
+ls -l deactivate
+```
+```bash
+chmod +x deactivate
+```
+```bash
+ls -l deactivate
+```
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-NLP-End-to-End"
+AUTHOR_USER_NAME = "heysourin"
+SRC_REPO = "nlpProject"
+AUTHOR_EMAIL = "sourin07@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for nlp app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+"""
+'setup.py' is a python file, the presence of which is an indication that the module/package you are about to install has likely been packaged and distributed with Distutils, which is the standard for distributing Python Modules.
+
+This allows you to easily install Python packages. Often it's enough to write:
+
+`$ pip install . `
+
+pip will use setup.py to install your module. Avoid calling setup.py directly.
+"""
